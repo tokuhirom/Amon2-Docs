e2065aa0f8246ea8874cea2a2a32c27f130f59ef
tokuhirom <tokuhirom@gmail.com>
2013-02-09 13:14:10 +0900

    updated.

--- source/amon3.rst
+++ source/amon3.rst
@@ -17,3 +17,8 @@ Amon2 uses ``$ENV{PLACK_ENV}`` as default configuration name.
 
 But it's bad idea. It makes hard to use. Amon2 should use ``$ENV{AMON2_ENV}`` or ``$ENV{"${PROJ}_ENV"}`` instead.
 
+Context
+-------
+
+Current version of Amon2 uses $Amon2::CONTEXT as a global variable.
+I want to make it as project local variable.
--- source/conf.py
+++ source/conf.py
@@ -49,8 +49,8 @@ copyright = u'2011, tokuhirom'
 # built documents.
 #
 # The short X.Y version.
-# version = subprocess.Popen(['perl', '-e' 'use Amon2; print $Amon2::VERSION'], stdout=subprocess.PIPE).communicate()[0]
-version='3.27'
+version = subprocess.Popen(['perl', '-e' 'use Amon2; print $Amon2::VERSION'], stdout=subprocess.PIPE).communicate()[0]
+# version='3.27'
 # The full version, including alpha/beta/rc tags.
 release = version
 
