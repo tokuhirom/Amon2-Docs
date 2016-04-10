952aed9d0c4244a261933e8c0008640485c2fa78
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-04-10 09:47:35 +0900

    docs

--- install.sh
+++ install.sh
@@ -1,4 +1,2 @@
-# python-sphinx
-yum groupinstall -y'Development Tools'
-yum install -y python-sphinx perl-autodie perl-Plack perl-App-cpanminus perl-IPC-System-Simple perl-Module-Build-Tiny perl-TAP-Harness-Env
-cpanm -n Amon2
+#!/bin/bash
+yum install -y python-sphinx
