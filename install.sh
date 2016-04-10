c0b8cff93cc8e33841108ba7da19d6e1a244c1cc
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-04-10 09:32:27 +0900

    no tests

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 # python-sphinx
 yum install -y python-sphinx perl-autodie perl-Plack perl-App-cpanminus perl-IPC-System-Simple perl-Module-Build-Tiny perl-TAP-Harness-Env
-cpanm Amon2
+cpanm -n Amon2
