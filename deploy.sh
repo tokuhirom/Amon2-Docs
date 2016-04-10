ea2f8f35d7e1bbe37280747955dfcdf9b5bff244
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-04-10 09:33:47 +0900

    remove useless comments

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-# install latest Amon2
-# cpanm -l ~/perl5/ --notest --no-man-pages JSON
-# cpanm -l ~/perl5/ --notest --no-man-pages `perl -E 'use JSON; say decode_json(qx{curl http://api.beta.metacpan.org/release/Amon2})->{download_url}'`
-
 make html
